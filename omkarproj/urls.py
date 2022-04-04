8df7c2e5a1af5195f85e7f3d458c8eda7f1ef161
Omkar <komarpant009@gmail.com>
2022-04-04 19:56:01 +0530

    second commit

--- omkarproj/urls.py
+++ omkarproj/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('wiz.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
